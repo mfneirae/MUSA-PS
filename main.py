7d5c7126776018edab4bf59e3ee902a1813e50a9
Manuel <mfneirae@unal.edu.co>
2019-02-09 21:35:56 -0500

    First Draw

--- main.py
+++ main.py
@@ -1,1 +1,16 @@
+#
+# #############################################################################
+#       Copyright (c) 2019 mfneirae.com All Rights Reserved.
+#
+#                This work  is licensed under a Creative Commons
+#       Attribution-NonCommercial - ShareAlike 4.0 International
+#       License and MIT Licence.
+#
+#       by Manuel Neira Embus.
+#
+#       For more information write me to jai@mfneirae.com
+#       Or visit my webpage at https://mfneirae.com/
+# #############################################################################
+#
+
 import numpy as np
