2ecd81f004f820b0a386ab183adf37035faaf96b
Manuel Embus <mfneirae@unal.edu.co>
2019-03-03 19:15:33 -0500

    Change to an object

--- js/box.js
+++ js/box.js
@@ -13,11 +13,88 @@
 // #       Or visit my webpage at https://mfneirae.com/
 // # #############################################################################
 // #
-//
+// *****************************************************************************
+// BLOBAL VARIABLES
+// *****************************************************************************
+var a;
+var svgDoc;
+var ID;
+var IDTEXT;
+var EST;
+var EFT;
+var CODE;
+var LST;
+var DURATION;
+var LFT;
+var ATTRIBUTES;
+var ATTRIBUTESTEXT;
+var OPERATIONS;
+var OPERATIONSTEXT;
+// *****************************************************************************
+// START
+// *****************************************************************************
 window.onload=function() {
-var a = document.getElementById('svgObject');
-var svgDoc = a.contentDocument;
-var svgItem = svgDoc.getElementById("rect10-3-9-6");
-svgItem.setAttribute('stroke', 'red');
-svgItem.setAttribute('fill', 'none');
+a = document.getElementById('svgObject');
+svgDoc = a.contentDocument;
+ID = svgDoc.getElementById("ID-0001");
+IDTEXT = svgDoc.getElementById("IDTEXT-0001");
+EST = svgDoc.getElementById("EST-0001");
+EFT = svgDoc.getElementById("EFT-0001");
+CODE = svgDoc.getElementById("CODE-0001");
+LST = svgDoc.getElementById("LST-0001");
+DURATION = svgDoc.getElementById("DURATION-0001");
+LFT = svgDoc.getElementById("LFT-0001");
+ATTRIBUTES = svgDoc.getElementById("ATTRIBUTES-0001");
+ATTRIBUTESTEXT = svgDoc.getElementById("ATTRIBUTESTEXT-0001");
+OPERATIONS = svgDoc.getElementById("OPERATIONS-0001");
+OPERATIONSTEXT = svgDoc.getElementById("OPERATIONSTEXT-0001");
+//Stroke
+ID.setAttribute('stroke', 'black');
+ID.setAttribute('stroke-width', '3');
+EST.setAttribute('stroke', 'black');
+EST.setAttribute('stroke-width', '3');
+EFT.setAttribute('stroke', 'black');
+EFT.setAttribute('stroke-width', '3');
+CODE.setAttribute('stroke', 'black');
+CODE.setAttribute('stroke-width', '3');
+DURATION.setAttribute('stroke', 'black');
+DURATION.setAttribute('stroke-width', '3');
+LST.setAttribute('stroke', 'black');
+LST.setAttribute('stroke-width', '3');
+LFT.setAttribute('stroke', 'black');
+LFT.setAttribute('stroke-width', '3');
+ATTRIBUTES.setAttribute('stroke', 'black');
+ATTRIBUTES.setAttribute('stroke-width', '3');
+OPERATIONS.setAttribute('stroke', 'black');
+OPERATIONS.setAttribute('stroke-width', '3');
+//Fill
+ID.setAttribute('fill', 'none');
+EST.setAttribute('fill', 'none');
+EFT.setAttribute('fill', 'none');
+CODE.setAttribute('fill', 'none');
+DURATION.setAttribute('fill', 'none');
+LST.setAttribute('fill', 'none');
+LFT.setAttribute('fill', 'none');
+ATTRIBUTES.setAttribute('fill', 'none');
+OPERATIONS.setAttribute('fill', 'none');
+//Text
+ATTRIBUTESTEXT.setAttribute('font-size', '8');
+OPERATIONSTEXT.setAttribute('font-size', '8');
 };
+// *****************************************************************************
+// MUSA-BOX
+// *****************************************************************************
+
+document.getElementById('svgObject').onmouseover = function() {mouseOver()};
+document.getElementById('svgObject').onmouseout = function() {mouseOut()};
+
+function mouseOver() {
+  ID.setAttribute('fill', '#FFCB3F');
+  IDTEXT.setAttribute('fill', '#2F5567');
+}
+
+function mouseOut() {
+  ID.setAttribute('stroke', 'black');
+  IDTEXT.setAttribute('fill', 'black');
+  ID.setAttribute('fill', 'none');
+}
--- main.py
+++ main.py
@@ -12,12 +12,37 @@
 #       Or visit my webpage at https://mfneirae.com/
 # #############################################################################
 #
-
 import numpy as np
 import os
 
-os.chdir("images")
+MUSABOX = []
+MUSABOX.append('\
+<svg xmlns:dc="http://purl.org/dc/elements/1.1/"\n \
+  xmlns:cc="http://creativecommons.org/ns#"\n \
+  xmlns:rdf="http://www.w3.org/1999/02/22-rdf-syntax-ns#"\n \
+  xmlns:svg="http://www.w3.org/2000/svg"\n \
+  xmlns="http://www.w3.org/2000/svg"\n \
+  xmlns:sodipodi="http://sodipodi.sourceforge.net/DTD/sodipodi-0.dtd"\n \
+  xmlns:inkscape="http://www.inkscape.org/namespaces/inkscape"\n'
+  + '   width="' + str(100) +'"\n \
+  height="100">\n \
+  <circle cx="50" cy="50" r="40" stroke="green" stroke-width="4" fill="yellow" />\n \
+</svg>')
 
 
-os.chdir("../")
-os.system("savepng.py")
+# *****************************************************************************
+# Write File
+# *****************************************************************************
+f = open ("images/MUSA-BOXTEST.svg", "w")
+
+for item in MUSABOX:
+    try:
+        f.write(item)
+    except UnicodeEncodeError:
+        pass
+
+
+f.close()
+
+# os.chdir("images")
+# os.chdir("../")
